a4800e8ab6145d43f5e4e95bee3b8821252a161b
Yumi_Kawai <yycharlottetown@gmail.com>
2023-12-03 23:41:04 -0400

    Make it a function to call it later

--- random_image.py
+++ random_image.py
@@ -3,14 +3,17 @@ import random
 
 folder_path = "test_images"
 
-# List all files and find png files 
-file_list = os.listdir(folder_path)
-png_files = [file for file in file_list if file.lower().endswith(".png")]
+# List all files and find png files from test_images folder
+def random_image():
+    file_list = os.listdir(folder_path)
+    png_files = [file for file in file_list if file.lower().endswith(".png")]
 
-if png_files:
-    random_file = random.choice(png_files)
-    random_file_path = os.path.join(folder_path, random_file)
+    if png_files:
+        random_file = random.choice(png_files)
+        random_file_path = os.path.join(folder_path, random_file)
 
-    print("chosen file:", random_file_path)
-else:
-    print("No PNG files")
+        print("chosen file:", random_file_path)
+    else:
+        print("No PNG files")
+
+random_image()
